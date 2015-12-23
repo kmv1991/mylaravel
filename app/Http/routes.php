774ee65877138b26bf4ab85e36d517a75f303c5f
kmv1991 <kmv1991@bk.ru>
2015-12-23 13:58:17 +0300

    'hhh'

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('about', function () {
+    return view('about');
+});
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
